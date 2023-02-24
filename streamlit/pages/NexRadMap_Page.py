f5a91c1984003b76aaec88afbddcf5f7d8b821da
Meet Doshi <doshi.mee@northeastern.edu>
2023-02-24 17:51:10 -0500

    Update NexRadMap_Page.py

--- streamlit/pages/NexRadMap_Page.py
+++ streamlit/pages/NexRadMap_Page.py
@@ -10,27 +10,18 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 if 'username' not in st.session_state:
     st.session_state.username = ''
 
-if 'logout_disabled' not in st.session_state:
-    st.session_state.logout_disabled = True
-
 if 'logged_in' not in st.session_state:
     st.session_state.logged_in = False
 
 with st.sidebar:
     user = "Not Logged In" if st.session_state.username == "" else st.session_state.username
     st.write(f'Current User: {user}')
-    logout_button = st.button('LogOut', disabled=st.session_state.logout_disabled)
+    logout_button = st.button('Log Out')
     if logout_button:
-        for key in st.session_state.keys():
-            if key == 'login_disabled' or key == 'logout_disabled' or key == 'register_disabled':
-                st.session_state[key] = not st.session_state[key]
-            else:
-                st.session_state[key] = ''
-        st.session_state.login_disabled = False
-        st.session_state.register_disabled = False
+        st.session_state['logged_in'] = False
         st.experimental_rerun()
 
-if not st.session_state.username == "" and "access_token" in st.experimental_get_query_params():
+if st.session_state['logged_in'] == True:
     st.markdown("<h2 style='text-align: center;'>NexRad Map Geo-Locations 📍</h1>", unsafe_allow_html=True)
     
     BASE_URL = "http://localhost:8000"
@@ -48,8 +39,8 @@ if not st.session_state.username == "" and "access_token" in st.experimental_get
         map = folium.Map(location=[40,-100], tiles="OpenStreetMap", zoom_start=4)
         for i in range(0,len(map_dict)):
             folium.Marker(
-            location = [map_dict.iloc[i]['latitude'], map_dict.iloc[i]['longitude']],
-            popup = (map_dict.iloc[i]['Station_Code'],map_dict.iloc[i]['County'])
+            location = [map_dict['latitude'][i], map_dict['longitude'][i]],
+            popup = (map_dict['Station_Code'][i],map_dict['County'][i])
             ).add_to(map)
 
         st.markdown("<h2 style='text-align: center;'>Nexrad Station Pointers</h1>", unsafe_allow_html=True)
